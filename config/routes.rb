92f60dcdfb3c2b74edff8fc6859cc1960a031f5f
Eva Navarro <emanao@me.com>
2020-05-14 20:55:12 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#home'
   get '/auth/facebook/callback' => 'sessions#create'
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
 
 
   # The priority is based upon order of creation: first created -> highest priority.
